8b07558ea96ffb359a88cabd9ee9245f167953de
Thai Nguyen <privat_thai_nguyen@hotmail.com>
2024-05-08 14:38:03 +0300

    Added filetypes field and listing to the UI

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,53 @@
 
 import * as vscode from 'vscode';
+import addHeaders from './addHeaders';
 
 const contents = (webview: vscode.Webview): string => {
-	const { cspSource } = webview;
-
 	return `
 		<!DOCTYPE html>
 		<html lang="en">
 			<head>
 				<meta charset="UTF-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				
 				<title>Cat Coding</title>
 			</head>
 			<body>
 				<script>
 					console.log("abc");
 					const vscode = acquireVsCodeApi();
+					const selectedFileTypes = [];
+
+					const addFileType = () => {
+						const fileTypesList = document.getElementById("selected-file-types-list");
+						const fileTypesField = document.getElementById("file-types-field");
+					
+						const newType = fileTypesField.value;
+
+						if(selectedFileTypes.includes(newType)){
+							return;
+						}
+
+						selectedFileTypes.push(newType);
+
+						const newListItem = document.createElement("li");
+						newListItem.innerHTML = newType;
+
+						fileTypesList.appendChild(newListItem);
+					}
 				
-					const handleRun = (e) => {
+					const handleRun = () => {
 						console.log("DEF");
 						
 						const textBlockField = document.getElementById("text-block-field");
-						const targetPathField = document.getElementById("target-path-field");
+						const rootPathField = document.getElementById("root-path-field");
 						const skipItemsField = document.getElementById("skip-items-field");
-
-						console.log("textBlockField", textBlockField.value);
-						console.log("targetPathField", targetPathField.value);
-						console.log("skipItemsField", skipItemsField.value);
 
 						vscode.postMessage({
 							command: 'run',
 							data: {
 								textBlockFieldValue: textBlockField.value,
-								targetPathFieldValue: targetPathField.value,
+								fileTypesField: selectedFileTypes,
+								rootPathFieldValue: rootPathField.value,
 								skipItemsFieldValue: skipItemsField.value 
 							}
 						})
@@ -43,8 +57,14 @@ const contents = (webview: vscode.Webview): string => {
 				<label>Text block</label>
 				<textarea id="text-block-field" width="500" height="200"></textarea>
 				
-				<label>Target path:</label>
-				<input id="target-path-field" type="text" defaultValue="" />
+				<label>File Types</label>
+				<ul id="selected-file-types-list">
+				</ul>
+				<input id="file-types-field" type="text" defaultValue="" />
+				<button onClick="addFileType()">Add</button>
+
+				<label>Root path:</label>
+				<input id="root-path-field" type="text" defaultValue="" />
 
 				<label>Skip items:</label>
 				<textarea id="skip-items-field" width="500" height="200"></textarea>
@@ -64,7 +84,7 @@ export function activate(ctx: vscode.ExtensionContext) {
 	let panel: vscode.WebviewPanel | undefined;
 	// Find out whether or not this panel is in the background or not
 	const activeInColumn: vscode.ViewColumn | undefined = vscode.window.activeTextEditor && vscode.window.activeTextEditor.viewColumn;
-	const prependHeaderCMD = commands.registerCommand("vscode-header-plugin.testcmd", async () => {
+	const addHeadersCMD = commands.registerCommand("vscode-header-plugin.add-headers-to-files", async () => {
 
 		// If panel exists then, then focus on it.
 		if (panel) {
@@ -79,10 +99,13 @@ export function activate(ctx: vscode.ExtensionContext) {
 			panel.webview.html = contents(panel.webview);
 			console.log("ABC");
 			const messageHandler = (e: any): void => {
+				console.log("BBBB", e);
 				const { command, data } = e;
 
 				if (command === "run") {
 					// Start appending headers
+					addHeaders(data);
+
 				} else if (command === "save") {
 					// Save settings for later use
 				}
@@ -97,7 +120,7 @@ export function activate(ctx: vscode.ExtensionContext) {
 
 	});
 
-	ctx.subscriptions.push(prependHeaderCMD);
+	ctx.subscriptions.push(addHeadersCMD);
 }
 
 // This method is called when your extension is deactivated
