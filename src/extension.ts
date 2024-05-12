b06deb9112d0beebec5db5c37febe86008230a1e
Thai Nguyen <privat_thai_nguyen@hotmail.com>
2024-05-12 21:52:26 +0300

    Added and renamed certain commands. Plugin now reads settings from json files

--- src/extension.ts
+++ src/extension.ts
@@ -12,18 +12,21 @@ export function activate(ctx: vscode.ExtensionContext) {
 	let panel: vscode.WebviewPanel | undefined;
 	// Find out whether or not this panel is in the background or not
 
-	const addHeadersCMD = commands.registerCommand("vscode-header-plugin.add-headers-to-files-ui", () => {
+	const addHeadersCMDLegacy = commands.registerCommand("vscode-header-plugin.add-headers-to-files-LEGACY", () => {
 
 		// If panel exists then, then focus on it.
 
 		AddHeaderPanel.render(ctx);
 	});
 
-	const runHeader = commands.registerCommand("vscode-header-plugin.add-headers-to-files", () => {
+	const addHeadersCMD = commands.registerCommand("vscode-header-plugin.add-headers-to-files", () => {
+		const { fs, rootPath } = workspace;
 
+		const configFileUri: vscode.Uri = Uri.file(`${rootPath}/headerConfig.json`);
+		workspace.fs.readFile(configFileUri);
 	});
 
-	ctx.subscriptions.push(addHeadersCMD);
+	ctx.subscriptions.push(addHeadersCMDLegacy);
 }
 
 // This method is called when your extension is deactivated
