c651ece432ee115d9a2c7006d3c3d0cf8793ef29
Thai Nguyen <privat_thai_nguyen@hotmail.com>
2024-05-13 10:18:12 +0300

    Adjusted eslint settings and removed UI related files

--- esbuild.js
+++ esbuild.js
@@ -17,46 +17,15 @@ const extensionConfig = {
     external: ["vscode"],
 };
 
-const stylingConfig = {
-    ...baseConfig,
-    platform: "node",
-    mainFields: ["module", "main"],
-    format: "cjs",
-    entryPoints: ["./src/styling/header_panel.css"],
-    outfile: "./out/style.css",
-    external: ["vscode"],
-};
-
 (async () => {
     try {
         await build(extensionConfig);
-        await build(stylingConfig);
         console.log("build complete");
     } catch (err) {
         process.stderr.write(err.stderr);
         process.exit(1);
     }
 })();
-
-const webviewConfig = {
-    ...baseConfig,
-    watch: {
-        onRebuild(error, result) {
-            console.log("[watch] build started");
-            if (error) {
-                error.errors.forEach(error =>
-                    console.error(`> ${error.location.file}:${error.location.line}:${error.location.column}: error: ${error.text}`)
-                );
-            } else {
-                console.log("[watch] build finished");
-            }
-        },
-    },
-    target: "es2020",
-    format: "esm",
-    entryPoints: ["./src/webview/main.ts"],
-    outfile: "./out/webview.js",
-};
 
 (async () => {
     const args = process.argv.slice(2);
@@ -68,15 +37,10 @@ const webviewConfig = {
                 ...extensionConfig,
                 ...watchConfig,
             });
-            await build({
-                ...webviewConfig,
-                ...watchConfig,
-            });
             console.log("[watch] build finished");
         } else {
             // Build extension and webview code
             await build(extensionConfig);
-            await build(webviewConfig);
             console.log("build complete");
         }
     } catch (err) {
