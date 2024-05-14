8aeea81d81e265706475c931f282e4cb211ab664
Thai Nguyen <privat_thai_nguyen@hotmail.com>
2024-05-14 17:17:36 +0300

    First extension.test.ts prototype

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,44 +2,8 @@ const assert = require("assert");
 const vscode = require("vscode");
 
 suite('Test Simple Header Plugin for VSCode', () => {
-    const { Uri, commands, workspace } = vscode;
-
-    class TextDecoder {
-        constructor() { }
-
-        public decode(arg: string) {
-            return {
-                "startDirs": [
-                    "/src",
-                    "/out"
-                ],
-                "headerText": [
-                    "/**",
-                    "*",
-                    "*",
-                    "*  Copyright (c) 2024, <Firstname Lastname>, MIT License",
-                    "*  For more information, see the LICENSE file",
-                    "*",
-                    "*",
-                    "**/",
-                    "\n",
-                    "\n"
-                ],
-                "fileTypes": [
-                    "tsx",
-                    "ts",
-                    "js",
-                    "jsx",
-                    "css",
-                    "sass",
-                    "scss"
-                ],
-                "ignoreItems": [
-                    "/src/extension.ts"
-                ]
-            }
-        }
-    }
+    const { commands, workspace, fs, Uri } = vscode;
+    const { rootPath } = workspace;
 
     test("'Simple Header: Add headers to files' appends textblock to documents", () => {
         commands.executeCommand("vscode-header-plugin.add-headers-to-files");
@@ -48,6 +12,11 @@ suite('Test Simple Header Plugin for VSCode', () => {
         //
         //isExportDeclaration()
 
-        assert.strictEqual(1, 1);
+        const fileUri = Uri.file(`${rootPath}/src/extension.ts`);
+        const contents = fs.readFile(fileUri);
+
+        const data = new TextDecoder().decode(contents);
+
+        assert.ok(data.includes("Copyright"));
     });
 });
