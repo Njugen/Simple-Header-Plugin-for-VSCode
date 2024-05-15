f1c385c09d1776c3eac5bad13af71b2e29f884af
Thai Nguyen <privat_thai_nguyen@hotmail.com>
2024-05-15 16:01:36 +0300

    Updated package.json for bundling

---
+++ out/addHeaders.js
@@ -0,0 +1,490 @@
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
+/**
+*
+*
+*  Copyright (c) 2024, <Firstname Lastname>, MIT License
+*  For more information, see the LICENSE file
+*
+*
+**/
+
+
